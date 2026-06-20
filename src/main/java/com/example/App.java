774d733a8047f13e484f9c8491dc0a8c830eb251
SYpiece <105969639+SYpiece@users.noreply.github.com>
2026-06-20 12:53:08 +0800

    update

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,12 +1,50 @@
 package com.example;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.util.Objects;
 import java.util.Random;
 
 public class App {
     static native int call(int a, int b);
 
     static {
-        System.loadLibrary("app");
+        String os = System.getProperty("os.name").toLowerCase();
+        String resourcePath;
+        String libFileName;
+        if (os.contains("win")) {
+            resourcePath = "/native/app.dll";
+            libFileName = "app.dll";
+        } else if (os.contains("mac")) {
+            resourcePath = "/native/libapp.dylib";
+            libFileName = "libapp.dylib";
+        } else { // Linux / Unix
+            resourcePath = "/native/libapp.so";
+            libFileName = "libapp.so";
+        }
+
+        File tempDir = new File(System.getProperty("java.io.tmpdir"));
+        File tempFile = new File(tempDir, libFileName);
+        if (tempFile.exists()) {
+            tempFile.delete();
+        }
+        try (
+                InputStream in = Objects.requireNonNull(App.class.getResourceAsStream(resourcePath));
+                OutputStream out = Files.newOutputStream(tempFile.toPath());
+        ) {
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = in.read(buffer)) != -1) {
+                out.write(buffer, 0, len);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        tempFile.setExecutable(true);
+        tempFile.deleteOnExit();
+
+        System.load(tempFile.getAbsolutePath());
     }
 
     public static void main(String[] args) {
